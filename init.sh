9c0d8a570033db29befd473673a69e647174d99c
yangjunwei <DreamYangjw@outlook.com>
2021-01-16 03:29:17 +0800

    git add config

--- init.sh
+++ init.sh
@@ -9,4 +9,6 @@ git config --global user.email "DreamYangjw@outlook.com"
 git config --global user.name "yangjunwei"
 
 git config --global alias.lg "log --color --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' --abbrev-commit"
+git config --global alias.lggeturl "config --get remote.origin.url"
+git config --global alias.lgm "commit -m"
 ssh-keygen -t rsa -C "DreamYangjw@outlook.com"
