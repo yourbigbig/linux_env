f79387976ae8a0b2c29459624811a122dd77bd7d
yangjunwei <DreamYangjw@outlook.com>
2021-01-16 04:12:30 +0800

    fix git config

--- init.sh
+++ init.sh
@@ -1,14 +1,45 @@
 #!/bin/bash
-sudo apt-get install vim
+checksoft()
+{
+    res=(`whereis $1`)
+    # echo ${#res[*]}
+    if  [ ${#res[*]}} == 1 ]; then
+        sudo apt-get install $1
+    else
+        echo "$1 is  installed"
+    fi  
+}
+checksoft_isrun()
+{
+    ret=`ps -e |grep $1`
+    # echo $ret
+    if  [ -z "$ret" ]; then
+        sudo apt-get install $2
+    else
+        echo "$2 is  installed"
+    fi  
+}
+
+
+checksoft_isrun ssh openssh-server
+checksoft vim
+
+
 sudo cp .vimrc /etc/.vimrc
 sudo cp .vimrc ~/.vimrc
 
-sudo apt-get install openssh-server
+if [ ! -f ~/.ssh/id_rsa ];then
+ssh-keygen -t rsa -C "DreamYangjw@outlook.com"
+fi
+
+
 
 git config --global user.email "DreamYangjw@outlook.com"
 git config --global user.name "yangjunwei"
-
 git config --global alias.lg "log --color --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' --abbrev-commit"
 git config --global alias.lggeturl "config --get remote.origin.url"
-git config --global alias.lgm "commit -m"
-ssh-keygen -t rsa -C "DreamYangjw@outlook.com"
+git config --global alias.lgm "add . & git commit -m"
+git config --global alias.st "status"
+
+
+echo Config done
