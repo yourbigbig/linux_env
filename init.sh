903528fa4efa7246291e230dcb4554ec7265a597
yangjunwei <DreamYangjw@outlook.com>
2021-01-18 02:33:00 +0800

    fix vimrc

--- init.sh
+++ init.sh
@@ -1,12 +1,16 @@
 #!/bin/bash
+
+################################  your config   ##################################
 NAME="yangjunwei"
 EMAIL="DreamYangjw@outlook.com"
 
-echo **********************************
-echo Email:DreamYangJW@outlook.com
-echo github:https://github.com/yourbigbig/linux_env
-echo version:1.3
-echo ***********************************
+################################# author  info ###################################
+echo -**********************************
+echo -Email:DreamYangJW@outlook.com
+echo -https://github.com/yourbigbig/linux_env
+echo -version:1.3
+echo -***********************************
+#################################   functions   ###################################
 checksoft()
 {
     res=(`whereis $1`)
@@ -14,7 +18,7 @@ checksoft()
     if  [ ${#res[*]}} == 1 ]; then
         sudo apt-get install $1
     else
-        echo "$1 is  installed"
+        echo "$1 is already installed."
     fi  
 }
 checksoft_isrun()
@@ -28,23 +32,34 @@ checksoft_isrun()
     fi  
 }
 
+###################################################################################
 #sudo cp /etc/apt/sources.list /etc/apt/sources.list.bak
 sudo cp ./sources.list_aliyun /etc/apt/sources.list
 #sudo cp ./sources.list_aliyun /etc/apt/sources.list
 sudo apt-get update
 
+###############################  checksoft is exsit ################################
 checksoft_isrun ssh openssh-server
 checksoft vim
+checksoft gcc
+checksoft minicom
+checksoft git
 
-
+####################################################################################
+echo Config .vimrc.You can edit at ~/.vimrc and /etc/.vimrc.
+sed -i 's/Author:none/$NAME/g' .vimrc
 sudo cp .vimrc /etc/.vimrc
 sudo cp .vimrc ~/.vimrc
 
+#####################################################################################
 if [ ! -f ~/.ssh/id_rsa ];then
 ssh-keygen -t rsa -C "DreamYangjw@outlook.com"
+else
+	echo ssh-keygen is already config.
 fi
 
-
+#####################################################################################
+echo Config git alias.
 git config --global user.email "DreamYangjw@outlook.com"
 git config --global user.name "yangjunwei"
 git config --global alias.lg "log --color --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' --abbrev-commit"
@@ -56,5 +71,5 @@ git config --global alias.hr "--hard reset"
 git config --global alias.sr "--soft reset"
 git config --global alias.ck "checkout ."
 
+echo Config Done.Thank you for using.
 
-echo Config Done
---
+++ test.c
@@ -0,0 +1,10 @@
+/*************************************************************************
+   > File Name: test.c
+   > hahh
+   > mail: Dreamyangjw@outlook.com.com
+   > Descriptio: This is NULL
+   > Created Time: 2021-01-18
+ ************************************************************************/
+
+#include<stdio.h>
+
